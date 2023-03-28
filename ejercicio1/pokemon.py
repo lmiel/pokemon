7308f291bdb27defe839fe0b8f9cfe014aaf1081
lmiel <lucia.mielgo@gmail.com>
2023-03-29 00:55:36 +0200

    some more

--- ejercicio1/pokemon.py
+++ ejercicio1/pokemon.py
@@ -11,12 +11,12 @@ class pokemon():
         if not isinstance(Arma, Arma):
             raise TypeError("El Arma debe ser un arma")
         self.Ptos_salud = Ptos_salud
-        if not isinstance(Ptos_salud, int):
-            raise TypeError("El Id debe ser un entero")
+        if not isinstance(Ptos_salud, int) or Ptos_salud < 0 or Ptos_salud > 100:
+            raise TypeError("El Id debe ser un entero entre 0 y 100")
         self.Id_ataque = Id_ataque
-        if not isinstance(Id_ataque, int):
-            raise TypeError("El Id_ataque debe ser un entero")
+        if not isinstance(Id_ataque, int) or Id_ataque < 0 or Id_ataque > 10:
+            raise TypeError("El Id_ataque debe ser un entero entre 0 y 10")
         self.Id_denfesa = Id_denfesa
-        if not isinstance(Id_denfesa, int):
-            raise TypeError("El Id_denfesa debe ser un entero")
+        if not isinstance(Id_denfesa, int) or Id_denfesa < 0 or Id_denfesa > 10:
+            raise TypeError("El Id_denfesa debe ser un entero entre 0 y 10")
         
