e59bc2cd60788ff8ca2648f31b4f82d077da778e
lmiel <lucia.mielgo@gmail.com>
2023-03-29 22:41:03 +0200

    trabajamos en la file q es

--- Archivos_complementarios/weapon_type.py
+++ Archivos_complementarios/weapon_type.py
@@ -1,41 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from enum import Enum
 
-
-"""
-This Python module contains not only the Enum WeaponType, but also the test of
-this Python class.
-
-@contents :  This module contains not only a single Python class, but also the
-             test cases to probe its functionality.
-@project :  N/A
-@program :  N/A
-@file :  weapon_type.py
-@author :  Antonio Artes Garcia (antonio.artesgarcia@ceu.es)
-           Francisco Hernando Gallego (francisco.hernandogallego@ceu.es)
-           Ruben Juarez Cadiz (ruben.juarezcadiz@ceu.es)
-
-@version :  0.0.1, 08 November 2021
-@information :  The Zen of Python
-                  https://www.python.org/dev/peps/pep-0020/
-                Style Guide for Python Code
-                  https://www.python.org/dev/peps/pep-0008/
-                Example NumPy Style Python Docstrings
-                  http://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_numpy.html
-                doctest – Testing through documentation
-                  https://pymotw.com/2/doctest/
-
-@copyright :  Copyright 2021 GNU AFFERO GENERAL PUBLIC.
-              All rights are reserved. Reproduction in whole or in part is
-              prohibited without the written consent of the copyright owner.
-"""
-
-
-# Source packages.
-
-
-
-class WeaponType():
+class WeaponType(Enum):
+  
     """Python class to implement an enumeration for the attribute Weapon Type.
 
     This Python class implements an enumeration for the attribute Weapon Type.
