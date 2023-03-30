a3fdecefbba860e200445335ee04289ca67d64c3
lmiel <lucia.mielgo@gmail.com>
2023-03-31 00:45:23 +0200

    avances main

--- Archivos_complementarios/main.py
+++ Archivos_complementarios/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+from pokemon import Pokemon
+from weapon_type import WeaponType
 
 """
 This Python method contains the application of the Game.
@@ -29,65 +30,26 @@ This Python method contains the application of the Game.
               prohibited without the written consent of the copyright owner.
 """
 
-
 # Source packages.
 
-
-
 def get_data_from_user(name_file):
-    """Function to obtain data from each user.
-
-    This function obtains data from each user in order to set the configuration
-    of the Game.
-
-    Syntax
-    ------
-      [ ] = get_data_from_user(name_file)
-
-    Parameters
-    ----------
-      name_file str Name of the CSV file.
-
-    Returns
-    -------
-      list_pokemons List of Pokemons obtained from CSV .
-
-    Example
-    -------
-      >>> list_pokemons = get_data_from_user("file.csv")
-    """
-
-
+  lista_pokemon = []
+  file = open(name_file, "r")
+  for linea in file.readlines():
+    pokemon = linea.split(",") #para cada linea del fichero, separamos los datos por comas y los guardamos en una lista
+    lista_pokemon.append(Pokemon(pokemon[0], pokemon[1], pokemon[2], pokemon[3], pokemon[4], pokemon[5])) #id, pokemon_name, weapon_type, health_points, attack_rating, defense_rating
+  return lista_pokemon
 
 def get_pokemon_in_a_list_of_pokemons(coach_to_ask, list_of_pokemons):
-    """Function to know the list of Pokemons that are associated to the Coach.
-
-    This function is used in order to know the list of Pokemos that are
-    associated to the coach. This function prints the result of this list, so
-    the user can select a Pokemon.
-
-    Syntax
-    ------
-       [ ] = get_pokemon_in_a_list_of_pokemons(coach_to_ask, list_of_pokemons):
-
-    Parameters
-    ----------
-       [in] coach_to_ask Coach to ask for her/his list of Pokemons.
-       [in] list_of_pokemons List of the Pokemons that are associated to the
-                             coach.
-
-    Returns
-    -------
-       List List of the Pokemons associaated to the coach that are undefeated.
-
-    Example
-    -------
-       >>> get_pokemon_in_a_list_of_pokemons(1, list_of_pokemons)
-    """
-
+  lista_pokemon = list_of_pokemons.get(coach_to_ask)
+  for pokemon in lista_pokemon:
+    if not pokemon.is_alive():
+      lista_pokemon.remove(pokemon)
+  return lista_pokemon
 
 
 def coach_is_undefeated(list_of_pokemons):
+  
     """Function to know if the Coach is still undefeated.
 
     This function is used in order to know if the Coach is still undefeated.
@@ -133,43 +95,30 @@ def main():
     -------
       >>> main()
     """
-
     print("Welcome to the Game.")
     print("Let's start to set the configuration of each game user. \n")
 
     # Get configuration for Game User 1.
-
-
     # Get configuration for Game User 2.
-
 
     print("------------------------------------------------------------------")
     print("The Game starts...")
     print("------------------------------------------------------------------")
 
     # Get a copy of the list of pokemons:
-
-
     # Choose first pokemons
- 
-
     # Main loop.
-
-
 
     print("------------------------------------------------------------------")
     print("The Game has end...")
     print("------------------------------------------------------------------")
-
 
     print("------------------------------------------------------------------")
     print("Statistics")
     print("------------------------------------------------------------------")
     print("Game User 1:")
 
-
     print("Game User 2:")
-
 
 
 # Checking whether this module is executed just itself alone.
